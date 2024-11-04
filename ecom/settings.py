a42cab3f5bf116f237b0554552a5af39376b80ee
NgemiDaniel <135568142+NgemiDaniel@users.noreply.github.com>
2024-11-04 22:02:30 +0300

    Update settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['web-production-7144.up.railway.app']
 
 
 # Application definition
