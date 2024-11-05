663d2c9b2c2e6ea92d991ac477acd507c6b972e7
Danielngemi <dantezmalusi254@gmail.com>
2024-11-05 13:26:05 +0300

    python version

--- ecom/settings.py
+++ ecom/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
-from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,9 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 
-SECRET_KEY = config('DJANGO_SECRET_KEY')
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+SECRET_KEY ='django-insecure-@r#$b_+v1+^#ees@_ew+j*#7+8y$i4%^a797cd-&b@))006=p$'
+DEBUG =True
 
 ALLOWED_HOSTS = ['*']
 
